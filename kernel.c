f014857727e57a8d25b1751ccec9bb283cd2f075
Manu <Manu@MBP-de-Manu.iptv.microsoft.com>
2017-04-08 17:24:32 -0300

    Funcionaaa

--- kernel.c
+++ kernel.c
@@ -1,3 +1,4 @@
+
 void clear()
 {
   	char *vidptr = (char*)0xb8000; 	//donde está la memoria de video
@@ -15,38 +16,74 @@ void clear()
 
 void showstring(char* message)
 {
-
+	int tam_palabras[] ={133,64,53,65};
   	char *vidptr = (char*)0xb8000;//donde está la memoria de video
 	unsigned int i = 0;
 	unsigned int j = 0;
+	int p;
 	
 	while(message[j] != '\0') {
 		if(message[j]=='-')
 		{
-			//salto de linea 
+			
+			for(p=0;p<(tam_palabras[0]+4);p++){
+				vidptr[i]=' ';
+				vidptr[i+1]=0x08;
+				i+=2;
+			}
+			j++;
 		}
-		if(message[j]='>')//Tab = 4 espacios vacios
+		else if(message[j]=='/'){
+
+			for(p=0;p<(tam_palabras[1]+8);p++){
+				vidptr[i]=' ';
+				vidptr[i+1]=0x08;
+				i+=2;
+			}
+			j++;
+		}
+		else if(message[j]=='*'){
+
+			for(p=0;p<(tam_palabras[2]+8);p++){
+				vidptr[i]=' ';
+				vidptr[i+1]=0x08;
+				i+=2;
+			}
+			j++;
+		}
+		/*else if(message[j]='>')//Tab = 4 espacios vacios
 		{
 			vidptr[i] = ' ';
-			vidptr[i+2] = ' ';
-			vidptr[i+4] = ' ';
-			vidptr[i+6] = ' ';
-			j++;
-			i= i+8;
-		//creo que esta bien xD (a revisar y testear) ***
-		}
+			vidptr[i+1] = 0x08;
+			i+=2;
 
-		vidptr[i] = message[j];
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
+			vidptr[i] = ' ';
+			vidptr[i+1] = 0x08;
+			i+=2;
+
+			vidptr[i] = ' ';
+			vidptr[i+1] = 0x08;
+			i+=2;
+
+			vidptr[i] = ' ';
+			vidptr[i+1] = 0x08;
+			i+=2;
+
+			j++;
+		}*/
+
+			vidptr[i] = message[j];
+			vidptr[i+1] = 0x07;
+			++j;
+			i = i + 2;
+		
 	}
 }
 
 void main( void )
 {
-	int[] 
-    char *str = ">Tarea 1 Sistemas Operativos->>Integrantes:->>>Cristobal Caipillan->>>Manuel Martinez";
+	//int n[] = {41,30,30,30}; 
+    char *str = "Tarea 1 Sistemas Operativos-Integrantes:/Cristobal Caipillan*Manuel Martinez";
 	clear();
 	showstring(str);
  for(;;); /* Bucle para mantener el SO corriendo */
